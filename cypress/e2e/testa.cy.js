fdd26ebe63e3fb002404b7610a7ad9aa89b0bcb0
pabloZenledger <105517920+pabloZenledger@users.noreply.github.com>
2023-08-04 16:02:50 -0700

    update sample test for static page and added video recording

--- cypress/e2e/testa.cy.js
+++ cypress/e2e/testa.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https:pablozenledger.github.io')
+describe('simple test suite a for checking with CI/CD', () => {
+  it('simple static page check', () => {
+    cy.visit('https://pablozenledger.github.io/frontend-cicd/')
+    cy.wait(1500)
+    cy.get('#contact > .w3-center').should('contain.text','Contact')
   })
 })
